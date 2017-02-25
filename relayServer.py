28e6d9931232cc27af823f2a54dd05e68a2662b8
Ashish Timilsina <ashish_timilsina@comcast.com>
2017-02-25 15:24:00 -0500

    added relay functins and error handlers

--- relayServer.py
+++ relayServer.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 
-import SmartHome
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
 from time import time
 
+#Set pi to false when Pi is unavailable
+isPi = False
+
+#Import the smarthome module when if Pi is on
+if isPi is True:
+	import SmartHome
+
 PORT_NUMBER = 8080
+
 
 #This handles HTTP Requests
 class myHandler(BaseHTTPRequestHandler):
@@ -17,60 +24,108 @@ class myHandler(BaseHTTPRequestHandler):
 		relay.toggle(0)
 		relay.toggle(1)
 		self.wfile.write("{status:\"" + relay.status() + "\"")
+
 	#GET Requests
 	def do_GET(self):
+
+		#various Relay functions
+
+		#turns On the 2 outlets based on the channel # and if the pi is on
+		def relayOn(channel=None):
+			if isPi is False:
+				print("On: " + str(channel))
+			else:
+				if channel == 1:
+					relay.turnOn(1)
+				elif channel == 0:
+					relay.turnOn(0)
+				else:
+					relay.turnOn()
+
+		#turns off the 2 outlets based on the channel # and if the pi is on
+		def relayOff(channel=None):
+			if isPi is False:
+				print("Off: " + str(channel))
+			else:
+				if channel == 1:
+					relay.turnOff(1)
+
+				elif channel == 0:
+					relay.turnOff(0)
+				else:
+					relay.turnOff()
+
+		#toggles the 2 outlets based on the channel # and if the pi is on
+		def relayToggle(channel = None):
+		   if isPi is False:
+		       print("toggle: " + str(channel))
+		   else:
+		       if channel == 1:
+		           relay.toggle(1)
+		       elif channel == 0:
+		           relay.toggle(0)
+		       else:
+		           relay.toggle()
+
+
 		self.send_response(200)
 		self.send_header('Content-type', 'text/html')
 		self.end_headers()
 		print "Looking at ", self.path
-		#print self.client_address
-		#/on0
-		#/off0
-		#/toggle0
-		print self.path[1:4]
-		if (self.path[1:3] == "on"):
+
+
+		#sample path: /toggle/1 or /on/1
+		var1 = self.path.split("/")[1] #get the first value from the path after the first slash
+		var2 = self.path.split("/")[2] #get the second value from the path after the second slash
+
+
+		if (var1 == "on"):
 			try:
-				if (self.path[3] == "0"):
-					relay.turnOn(0)
+				if (var2 == "0"):
+					relayOn(0)
 				else:
-					relay.turnOn(1)
+					relayOn(1)
 			except:
-				relay.turnOn(0)
-				relay.turnOn(1)
-		elif (self.path[1:4] == "off"):
+				relayOn(0)
+				relayOn(1)
+		elif (var1 == "off"):
 			try:
-				print self.path[4]
-				if (self.path[4] == "0"):
-					relay.turnOff(0)
-				elif (self.path[4] == "1"):
-					relay.turnOff(1)
+
+				if (var2  == "0"):
+					relayOff(0)
+				elif (var2  == "1"):
+					relayOff(1)
 			except:
-				relay.turnOff(0)
-				relay.turnOff(1)
-		elif (self.path[1:7] == "toggle"):
+					relayOff(0)
+					relayOff(1)
+
+		elif (var1 == "toggle"):
 			try:
-				if (self.path[7] == "0"):
-					relay.toggle(0)
-				elif (self.path[7] == "1"):
-					relay.toggle(1)
+				if (var2 == "0"):
+					relayToggle(0)
+				elif (var2  == "1"):
+					relayToggle(1)
 			except:
-				relay.toggle(0)
-				relay.toggle(1)
-		elif (self.path[1:5] == "wave"):
-			relay.wave()
+				relayToggle(0)
+				relayToggle(1)
 		else:
-			print "idunno"
-		self.wfile.write(relay.status())
+			print "none"
+
+		if isPi is False:
+			self.wfile.write("Pi is False!")
+		else:
+			self.wfile.write("Relay Status: " + relay.status())
 		return
 
 try:
 	#Create web server and define the request handler
 	server = HTTPServer(('', PORT_NUMBER), myHandler)
 	print 'Started httpserver on port ' , PORT_NUMBER
-	gpio = SmartHome.Gpio()
-	relay = SmartHome.Relay([17, 27])
-	relay.wave()
-	
+
+	if isPi is True: #if Pi is on then call the Gpio and Relay functions
+		gpio = SmartHome.Gpio()
+		relay = SmartHome.Relay([17, 27])
+
 	server.serve_forever()
 
 except KeyboardInterrupt:
