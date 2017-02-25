a4e1057fdf6437a30004052e024163cf49ddc0fe
Alex Osman <aro39@drexel.edu>
2017-02-25 12:30:42 -0500

    format egbServer for linting

--- rgbServer.py
+++ rgbServer.py
@@ -1,68 +1,68 @@
 #!/usr/bin/python
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from os import curdir, sep
-from time import time
 
 isPi = False
 
 if isPi:
-	import SmartHome
+    import SmartHome
 PORT_NUMBER = 8081
 
 
 def changeColor(rgb):
-	if isPi:
-		rgb.changeColor(rgb)
-	else:
-		print("rgb::color ", rgb)
+    if isPi:
+        rgb.changeColor(rgb)
+    else:
+        print("rgb::color ", rgb)
+
 
 def changeStyle(style):
-	if isPi:
-		rgb.changeStyle(style)
-	else:
-		print("rgb::style", style)
+    if isPi:
+        rgb.changeStyle(style)
+    else:
+        print("rgb::style", style)
 
 
-#This handles HTTP Requests
+# This handles HTTP Requests
 class myHandler(BaseHTTPRequestHandler):
-	#GET Requests
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header('Content-type', 'text/html')
-		self.end_headers()
-		
-		#Remove the leading '/'
-		self.path = self.path[1:]
+    # GET Requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-		#/{style}
-		#/{r}/{g}/{b}
-		#/{style}/{r}/{g}/{b}
-		args = self.path.split('/')
-		if len(args) == 1:
-			changeStyle(args[0])
-		elif len(args) == 3:
-			changeColor([args[0], args[1], args[2]])
-		elif len(args) == 4:
-			changeStyle(args[0])
-			changeColor([args[1], args[2], args[3]])
-		else:
-			print "Unknown path"
+        # Remove the leading '/'
+        self.path = self.path[1:]
 
+        # /{style}
+        # /{r}/{g}/{b}
+        # /{style}/{r}/{g}/{b}
+        args = self.path.split('/')
+        if len(args) == 1:
+            changeStyle(args[0])
+        elif len(args) == 3:
+            changeColor([args[0], args[1], args[2]])
+        elif len(args) == 4:
+            changeStyle(args[0])
+            changeColor([args[1], args[2], args[3]])
+        else:
+            print "Unknown path"
 
-		self.wfile.write("Okay");
-		#self.wfile.write(rgb.brightness)
-		return
+        self.wfile.write("Okay")
+        # self.wfile.write(rgb.brightness)
+        return
+
 
 try:
-	#Create web server and define the request handler
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	if isPi:
-		gpio = SmartHome.Gpio()
-		rgb = SmartHome.rgb([26, 19, 13])	
-	server.serve_forever()
+    # Create web server and define the request handler
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ', PORT_NUMBER
+    if isPi:
+        gpio = SmartHome.Gpio()
+        rgb = SmartHome.rgb([26, 19, 13])
+    server.serve_forever()
+
 
 except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
+    print '^C received, shutting down the web server'
+    server.socket.close()
